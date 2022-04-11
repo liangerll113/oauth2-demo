2a91b680dfec688c2d554924df2ec40a2c3f8318
xinwodexie <916339469@qq.com>
2022-04-11 19:35:13 +0800

    新增refresh access_token功能

--- src/main/java/com/xwdx/oauth2demo/config/MyAuthorizationServerConfig.java
+++ src/main/java/com/xwdx/oauth2demo/config/MyAuthorizationServerConfig.java
@@ -1,5 +1,7 @@
 package com.xwdx.oauth2demo.config;
 
+import com.xwdx.oauth2demo.service.MyUserDetailService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.oauth2.config.annotation.configurers.ClientDetailsServiceConfigurer;
@@ -15,19 +17,24 @@ import org.springframework.security.oauth2.config.annotation.web.configurers.Aut
 @EnableAuthorizationServer
 public class MyAuthorizationServerConfig extends AuthorizationServerConfigurerAdapter {
 
+    @Autowired
+    private MyUserDetailService myUserDetailService;
+
     @Override
     public void configure(ClientDetailsServiceConfigurer clients) throws Exception {
         clients.inMemory().withClient("client")
                 .redirectUris("http://www.xwdx.site")
                 .scopes("read", "write")
                 .secret("secret")
-                .authorizedGrantTypes("authorization_code", "password", "implicit", "client_credentials");
+                .authorizedGrantTypes("authorization_code", "password", "implicit", "client_credentials","refresh_token");
     }
 
     @Override
     // 可以不配置，默认只支持post
     public void configure(AuthorizationServerEndpointsConfigurer endpoints) throws Exception {
-        endpoints.allowedTokenEndpointRequestMethods(HttpMethod.GET, HttpMethod.POST);
+        endpoints.allowedTokenEndpointRequestMethods(HttpMethod.GET, HttpMethod.POST)
+                .reuseRefreshTokens(false)
+                .userDetailsService(myUserDetailService);
     }
 
 }
